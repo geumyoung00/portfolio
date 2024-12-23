be882c46e1fa7b0b53f26c1c0dd3564a2f0d0d46
geumyoung <gyn093@elecocean.co.kr>
2024-12-23 16:53:26 +0900

    default setting

---
+++ src/common/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p className='copy'>Copyright ©2024 GEUMYOUNG. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/common/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>
+        <div className='logo'></div>
+        <span className='logoTitle'>NOH GEUMYOUNG</span>
+      </h1>
+      <nav className='navigation'>
+        <div className='menu'>
+          <span>INFO</span>
+        </div>
+        <div className='menu'>
+          <span>PROJECT</span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
