3d318e53fd1ce4ce050b6c3407d27c9f130995d5
젤리 <1gyn093@gmail.com>
2025-07-27 19:59:02 +0900

    update vite version

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import VitePluginInjectPreload from 'vite-plugin-inject-preload';
-import path from 'path';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    svgr(),
-    react(),
-    VitePluginInjectPreload({
-      files: [
-        {
-          match: /\b\w+\.woff2\b/,
-          attributes: {
-            rel: 'preload',
-            type: 'font/woff2',
-            as: 'font',
-            crossorigin: 'anonymous',
-          },
-        },
-      ],
-      injectTo: 'head-prepend',
-    }),
-  ],
+  plugins: [svgr(), react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@style': path.resolve(__dirname, 'src/style'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@style': fileURLToPath(new URL('./src/style', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
     },
   },
 });
